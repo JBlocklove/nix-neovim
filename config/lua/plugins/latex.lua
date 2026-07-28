315fa6cca5e6083d885f517b78928fbf75e16971
Jason Blocklove <jason.blocklove@gmail.com>
2026-07-28 13:18:26 -0400

    Suppressing annoying latex warnings

--- config/lua/plugins/latex.lua
+++ config/lua/plugins/latex.lua
@@ -12,13 +12,16 @@ return {
 
 		vim.g.vimtex_quickfix_mode = "2"
 		vim.g.vimtex_quickfix_open_on_warning = "0"
-		vim.g.vimtex_quickfix_ignore_filters = {
-			"Marginpar on page",
-			"Overfull \\hbox",
-			"Underfull \\hbox",
-		}
+
 		vim.g.vimtex_quickfix_autoclose_after_keystrokes = "10"
 
 		vim.g.vimtex_indent_enabled = "0"
+
+		vim.g.vimtex_quickfix_ignore_filters = {
+			[[Underfull \\hbox]], -- hide underfull hbox warnings
+			[[Overfull \\hbox]], -- (optional) hide overfull hbox warnings too
+			[[Underfull \\vbox]], -- (optional) hide underfull vbox warnings too
+
+		}
 	end,
 }
